859df22b3467988c2e1016164dc21c5c374d5958
vartankoko <vartan.koko@gmail.com>
2018-03-25 16:55:41 +0200

    Refactoring de tout les types de diplay

---
+++ Application/src/main/java/com/example/android/display/DisplayEcgImpl.java
@@ -0,0 +1,107 @@
+package com.example.android.display;
+
+import android.util.Log;
+
+import com.scichart.charting.model.dataSeries.XyDataSeries;
+import com.scichart.charting.visuals.SciChartSurface;
+import com.scichart.charting.visuals.renderableSeries.IRenderableSeries;
+import com.scichart.extensions.builders.SciChartBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import uk.me.berndporr.iirj.Butterworth;
+
+/**
+ * Created by kokoghlanian on 20/03/2018.
+ */
+
+public class DisplayEcgImpl implements IDisplayData {
+
+    private Butterworth mBtwFilterLow;
+    private Butterworth mBtwFilterHigh;
+
+    public DisplayEcgImpl() {
+
+        mBtwFilterLow = new Butterworth();
+        mBtwFilterLow.lowPass(4, 500, 40);
+        mBtwFilterHigh = new Butterworth();
+        mBtwFilterHigh.highPass(2, 500, 0.05);
+    }
+
+
+    @Override
+    public ArrayList<Double> getDataFromString(String data) {
+
+        ArrayList<Double> mDataList = new ArrayList<Double>();
+        if (data != null) {
+            String[] dataList = data.split("\n");
+            String dataDecoded = dataList[dataList.length - 1].replace(" ", "");
+            for (int i = 0; i < 30; i++) {
+                double mPoint = (double) Integer.parseInt(dataDecoded.substring(4 * i, 4 * i + 4), 16) * 2.4 / (32768 - 1);
+                if (mPoint > 2.4) {
+                    mDataList.add((-4.8 + mPoint));
+                } else {
+                    mDataList.add(mPoint);
+                }
+            }
+        }
+        return mDataList;
+    }
+
+    @Override
+    public ArrayList<Double> getDataWhithoutFilter(ArrayList<Double> dataList, int channelSelected){
+        ArrayList<Double> dataFromChannel = new ArrayList<>();
+        if(dataList.size()> 0){
+            for(int i = 0; i < 10; i++){
+                dataFromChannel.add(dataList.get(3 * i + channelSelected - 1));
+            }
+        }
+        return dataFromChannel;
+    }
+
+    @Override
+    public ArrayList<Double> getDataWithFilter(ArrayList<Double> dataList, int channelSelected){
+        ArrayList<Double> dataFromChannel = new ArrayList<>();
+        if(dataList.size()> 0){
+            for(int i = 0; i < 10; i++){
+                dataFromChannel.add(mBtwFilterLow.filter(mBtwFilterHigh.filter(dataList.get(3 * i + channelSelected - 1))));
+            }
+        }
+        return dataFromChannel;
+    }
+
+
+    /* Methode qui ne servira surement plus servait a récupéré les XyDataSeries, qui ne sont pas utile pour nos traitement
+
+    public XyDataSeries createDataFromDouble(boolean isFilteringOn, ArrayList<Double> mDataList, int channelSelected, int compteur) {
+
+        final SciChartBuilder builder = SciChartBuilder.instance();
+        XyDataSeries ecgData = builder.newXyDataSeries(Integer.class, Double.class).withFifoCapacity(15).build();
+
+        for (int j = 0; j < 10; j++) {
+            if (isFilteringOn) {
+                ecgData.append((compteur + j) * 2, mBtwFilterLow.filter(mBtwFilterHigh.filter(mDataList.get(3 * j + channelSelected - 1))));
+
+            } else {
+                ecgData.append((compteur + j) * 2, mDataList.get(3 * j + channelSelected - 1));
+            }
+
+        }
+        return ecgData;
+    }*/
+
+    @Override
+    public ArrayList<Double> displayData(String data, boolean isDataSave, int channelSelected, boolean isFilteringOn, int compteur ) {
+
+        ArrayList<Double> ecgData;
+        ArrayList<Double> dataList = this.getDataFromString(data);
+        if (isFilteringOn) {
+            //revoir gestion de la sauvegarde sur csv plus tard;
+            ecgData = getDataWhithoutFilter(dataList,channelSelected);
+        } else {
+            ecgData = getDataWithFilter(dataList,channelSelected);
+        }
+        return ecgData;
+    }
+}
---
+++ Application/src/main/java/com/example/android/display/DisplaySpO2Impl.java
@@ -0,0 +1,98 @@
+package com.example.android.display;
+
+import android.util.Log;
+
+import com.scichart.charting.model.dataSeries.XyDataSeries;
+
+import java.util.ArrayList;
+
+import uk.me.berndporr.iirj.Butterworth;
+
+/**
+ * Created by kokoghlanian on 20/03/2018.
+ */
+
+public class DisplaySpO2Impl implements IDisplayData {
+
+    private Butterworth mBtwFilterLow;
+    private Butterworth mBtwFilterHigh;
+    private double dataDecodedR1MSB;
+    private double dataDecodedR1LSB;
+    private double dataDecodedIr1MSB;
+    private double dataDecodedIr1LSB;
+    private double dataDecodedR2MSB ;
+    private double dataDecodedR2LSB ;
+    private double dataDecodedIr2MSB ;
+    private double dataDecodedIr2LSB ;
+
+
+    public DisplaySpO2Impl(){
+
+        mBtwFilterLow = new Butterworth();
+        mBtwFilterLow.lowPass(4, 500, 40);
+        mBtwFilterHigh = new Butterworth();
+        mBtwFilterHigh.highPass(2, 500, 0.05);
+    }
+
+    @Override
+    public ArrayList<Double> getDataFromString(String data) {
+     return null;
+    }
+
+    @Override
+    public ArrayList<Double> getDataWhithoutFilter(ArrayList<Double> dataList, int channelSelected) {
+
+        ArrayList<Double> spo2Data = new ArrayList<>();
+        if (channelSelected == 1) {
+            //double databuged = dataDecodedR1LSB + ((dataDecodedR1MSB % 2) + ((dataDecodedR1MSB - (dataDecodedR1MSB % 2)) % 4)) * 65536 + dataDecodedR2LSB + ((dataDecodedR2MSB % 2) + ((dataDecodedR2MSB - (dataDecodedR2MSB % 2)) % 4)) * 65536;
+
+            spo2Data.add(dataDecodedR1LSB + ((dataDecodedR1MSB % 2) + ((dataDecodedR1MSB - (dataDecodedR1MSB % 2)) % 4)) * 65536);
+            spo2Data.add(dataDecodedR2LSB + ((dataDecodedR2MSB % 2) + ((dataDecodedR2MSB - (dataDecodedR2MSB % 2)) % 4)) * 65536);
+        } else {
+            //double databuged = dataDecodedIr1LSB + ((dataDecodedIr1MSB % 2) + ((dataDecodedIr1MSB - (dataDecodedIr1MSB % 2)) % 4)) * 65536 + dataDecodedIr2LSB + ((dataDecodedIr2MSB % 2) + ((dataDecodedIr2MSB - (dataDecodedIr2MSB % 2)) % 4)) * 65536;
+
+            spo2Data.add(dataDecodedIr1LSB + ((dataDecodedIr1MSB % 2) + ((dataDecodedIr1MSB - (dataDecodedIr1MSB % 2)) % 4)) * 65536);
+            spo2Data.add(dataDecodedIr2LSB + ((dataDecodedIr2MSB % 2) + ((dataDecodedIr2MSB - (dataDecodedIr2MSB % 2)) % 4)) * 65536);
+        }
+        return spo2Data;
+    }
+
+    @Override
+    public ArrayList<Double> getDataWithFilter(ArrayList<Double> dataList, int channelSelected) {
+
+        ArrayList<Double> spo2Data = new ArrayList<>();
+        if (channelSelected == 1) {
+            spo2Data.add(mBtwFilterLow.filter(mBtwFilterHigh.filter(dataDecodedR1LSB + ((dataDecodedR1MSB % 2) + ((dataDecodedR1MSB - (dataDecodedR1MSB % 2)) % 4)) * 65536)));
+            spo2Data.add(mBtwFilterLow.filter(mBtwFilterHigh.filter(dataDecodedR2LSB + ((dataDecodedR2MSB % 2) + ((dataDecodedR2MSB - (dataDecodedR2MSB % 2)) % 4)) * 65536)));
+        } else {
+            spo2Data.add(mBtwFilterLow.filter(mBtwFilterHigh.filter(dataDecodedIr1LSB + ((dataDecodedIr1MSB % 2) + ((dataDecodedIr1MSB - (dataDecodedIr1MSB % 2)) % 4)) * 65536)));
+            spo2Data.add( mBtwFilterLow.filter(mBtwFilterHigh.filter(dataDecodedIr2LSB + ((dataDecodedIr2MSB % 2) + ((dataDecodedIr2MSB - (dataDecodedIr2MSB % 2)) % 4)) * 65536)));
+        }
+        return spo2Data;
+    }
+
+    @Override
+    public ArrayList<Double> displayData(String data, boolean isDataSave, int channelSelected, boolean isFilteringOn, int compteur) {
+        ArrayList<Double> spo2Data = new ArrayList<>();
+        if (data != null) {
+            final String[] dataList = data.split("\n");
+            final String dataDecoded = dataList[dataList.length - 1].replace(" ", "");
+            this.dataDecodedR1MSB = Integer.parseInt(dataDecoded.substring(173, 174), 16);
+            this.dataDecodedR1LSB = Integer.parseInt(dataDecoded.substring(174, 178), 16);
+            this.dataDecodedIr1MSB = Integer.parseInt(dataDecoded.substring(179, 180), 16);
+            this.dataDecodedIr1LSB = Integer.parseInt(dataDecoded.substring(180, 184), 16);
+            this.dataDecodedR2MSB = Integer.parseInt(dataDecoded.substring(185, 186), 16);
+            this.dataDecodedR2LSB = Integer.parseInt(dataDecoded.substring(186, 190), 16);
+            this.dataDecodedIr2MSB = Integer.parseInt(dataDecoded.substring(191, 192), 16);
+            this.dataDecodedIr2LSB = Integer.parseInt(dataDecoded.substring(192, 196), 16);
+
+            if (isFilteringOn) {
+                spo2Data = this.getDataWithFilter(null,channelSelected);
+            } else {
+                spo2Data = this.getDataWhithoutFilter(null,channelSelected);
+            }
+        }
+        return spo2Data;
+    }
+
+}
